e4405503f0ef19f8ebebed2f814f0cd3418ed6a5
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-08-02 12:18:02 +0530

    added week1 assign 3

--- week (1)/programming_assign_3.cpp
+++ week (1)/programming_assign_3.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
 
+std::stack<std::string> ss; //LINE-1
+void remove(){
+    ss.pop();       //LINE-2
+}
+
+int main() {
+    string str;
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> str;
+        ss.push(str);   //LINE-3
+    }
+
+    while(!ss.empty()) {
+
+        cout << ss.top() << " ";
+
+        remove();
+    }
+    return 0;
+}
